dd2ef595b69717c55d902af93a88ae30f63ed914
Jared Lyon <jaredalyon@gmail.com>
2020-04-21 23:09:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
 const bot = new Discord.Client(require("./config.json").opts);
 require('./funcs.js')(bot);
 const readdir = require("fs").readdir;
 
+bot.config = require("./config.json");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.memeCache = new Set();
