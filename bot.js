c1cbd0e7907228d628f05d67312e66e2b981b497
Jared Lyon <jaredalyon@gmail.com>
2020-04-26 04:23:39 -0700

    fix issue #2

--- bot.js
+++ bot.js
@@ -30,32 +30,6 @@ readdir('./events/', (err, files) => {
 	bot.log(`Events loaded!`);
 });
 
-//midnight meme timer
-let targetChannel1 = bot.guilds.cache.get(bot.config.arynServer.server).channels.cache.get(bot.config.arynServer.channel);
-let targetChannel2 = bot.guilds.cache.get(bot.config.jaredServer.server).channels.cache.get(bot.config.jaredServer.channel);
-let targetChannel3 = bot.guilds.cache.get(bot.config.mikalServer.server).channels.cache.get(bot.config.mikalServer.channel);
-
-var midnightMeme;
-bot.on('ready', () => {
-	midnightMeme = bot.channels.cache.get('701002021007917141');
-});
-
-const TARGET_HOUR_M = 0;
-const TARGET_MINUTE_M = 0;
-
-async function post(memeInput) {
-	await targetChannel1.send({ files: [memeInput] });
-	await targetChannel2.send({ files: [memeInput] });
-	await targetChannel3.send({ files: [memeInput] });
-}
-
-setInterval(function () {
-	var d1 = new Date();
-	if (d1.getMinutes() !== TARGET_MINUTE_M || d1.getHours() !== TARGET_HOUR_M) return;
-	bot.memeCache.forEach(post);
-	midnightMeme.send("[MIDNIGHT MEME] | Meme(s) successfully distributed!")
-}, 60 * 1000);
-
 //restart timer
 var restart;
 bot.on('ready', () => {
